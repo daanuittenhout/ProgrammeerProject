227c347d91bd15dd36fe48d0994372933a5520b0
daanuittenhout <daanuittenhout@gmail.com>
2018-01-12 14:17:11 +0100

    up

--- scripts/tax.js
+++ scripts/tax.js
@@ -11,9 +11,6 @@ var h = 500,
 var projection = d3.geo.mercator()
             .scale(1)
             .translate([3, 0]);
-var projection = d3.geo.equirectangular()
-			.scale(153)
-	    .translate([width/2,height/2])
 
 var path = d3.geo.path()
         .projection(projection);
@@ -21,7 +18,7 @@ var path = d3.geo.path()
 var svg = d3.select("#map").append("svg")
         .attr("height", h)
         .attr("width", fill)
-var g = svg.append("g");
+
 
 var svg1 = d3.select("#bar").append("svg")
         .attr("height", h)
@@ -31,11 +28,5 @@ var svg2 = d3.select("#flower").append("svg")
         .attr("height", h)
         .attr("width", fill);
 
-d3.json("scripts/world-countries-sans-antarctica.json", function(error, data) {
-  if (error) return console.error(error);
 
-
-
-
-});
 }
