3515373337a51564397f0bce5dae39a2c2f196fc
daanuittenhout <daanuittenhout@gmail.com>
2018-01-15 16:06:39 +0100

    hoi

--- data/convertCSV2JSON.py
+++ data/convertCSV2JSON.py
@@ -12,58 +12,87 @@ json_bar = {}
 
 json_map = {}
 
+for i in range(15):
+	year = str(2002 + i)
+	json_bar[year] = {}
 
-
-
+print(json_bar)
 
 # Convert data from csv to JSON object with given keys.
-with open("politicalindex.csv", 'r') as csvfile, open("dataMap.json", 'w') as jsonfile_map, open("dataBar.json", 'w') as jsonfile_map:
+with open("politicalindex.csv", 'r') as csvfile, open("dataMap1.json", 'w')	as jsonfile_map, open("dataBar1.json", 'w') as jsonfile_bar:
 	# reader = csv.DictReader(csvfile, fieldnames, delimiter=';')
-	reader = csv.reader(csvfile, delimiter=',')
+	reader = csv.DictReader(csvfile, delimiter='\t')
 
-	# Write to JSON file.
-	for count, row in enumerate(reader):
-		if count < 1:
-			continue
-
-
-		# print(row)
-		state_bar = {row[1]:{
-			"Population": row[3],
-			"Violent crime rate": row[9],
-			"Murder and nonnegligent Manslaughter rate" : row[10],
-			"Legacy rape rate": row[11],
-			"Robbery rate": row[12],
-			"Aggravated assault rate": row[13],
-
-
-			}
-			}
-		state_map = {row[1]: {
-			"Population": row[3],
-			"Violent crime total": row[4],
-			"Murder and nonnegligent Manslaughter": row[5],
-			"Legacy rape": row[6],
-			"Robbery": row[7],
-			"Aggravated assault": row[8],
-
-			}
-		}
-
-
-		try:
-			json_bar[row[0]].update(state_bar)
-
-			json_map[row[0]].update(state_map)
-
-		except KeyError:
-			json_bar[row[0]] = {}
-			json_bar[row[0]] = state_bar
-
-			json_map[row[0]] = {}
-			json_map[row[0]] = state_map
-
-
+	for row in reader:
+		print(row)
+		if row["Country"] == "Country":
+			k = "country"
+		if row["Series"] == "Series":
+			v = "series"
+		print(row["Country"])
+		current_series = row["Series"]
+		current_country = row["Country Cod"]
+		for i in range(15):
+			year = str(2002 + i)
+			value = row[year]
+			if current_country in json_bar[year].keys():
+				json_bar[year][current_country][current_series] = value
+			else:
+				json_bar[year][current_country] = {current_series: value}
+	print(json_bar)
+		# for item in row:
+		# 	print(item)
+		# 	if k != "country" and v !="series":
+        #
+		# 		dummy = reader.setdefault(k, )
+				# dummy = reader.setdefault()
+#
+# 	# Write to JSON file.
+# 	for count, row in enumerate(reader):
+# 		if count > 0:
+# 			continue
+# while row != ""
+# 	for row in range(0,4):
+# 		for i in len(row):
+# 			if i == 0 and range[0]:
+#
+#
+# 			state_bar = {if i == 0:{
+# 				"Population": row[3],
+# 				"Violent crime rate": row[9],
+# 				"Murder and nonnegligent Manslaughter rate" : row[10],
+# 				"Legacy rape rate": row[11],
+# 				"Robbery rate": row[12],
+# 				"Aggravated assault rate": row[13],
+#
+#
+# 			}
+# 			}
+# 		state_map = {row[1]: {
+# 			"Population": row[3],
+# 			"Violent crime total": row[4],
+# 			"Murder and nonnegligent Manslaughter": row[5],
+# 			"Legacy rape": row[6],
+# 			"Robbery": row[7],
+# 			"Aggravated assault": row[8],
+#
+# 			}
+# 		}
+#
+#
+# 		try:
+# 			json_bar[row[0]].update(state_bar)
+#
+# 			json_map[row[0]].update(state_map)
+#
+# 		except KeyError:
+# 			json_bar[row[0]] = {}
+# 			json_bar[row[0]] = state_bar
+#
+# 			json_map[row[0]] = {}
+# 			json_map[row[0]] = state_map
+    #
+    #
 	json.dump(json_bar, jsonfile_bar, indent=2)
-
-	json.dump(json_map, jsonfile_map, indent=2)
+    #
+	# json.dump(json_map, jsonfile_map, indent=2)
--- scripts/tax.js
+++ scripts/tax.js
@@ -1,3 +1,5 @@
+
+
 window.onload = function(e){
 
 var body = d3.select("body")
@@ -8,19 +10,20 @@ var h = 500,
     w = 1000;
     fill = "100%"
 
-var projection = d3.geo.mercator()
-            .scale(1)
-            .translate([3, 0]);
+// var projection = d3.geo.mercator()
+//         .scale(1)
+//         .translate([3, 0]);
+//
+// var path = d3.geo.path()
+//         .projection(projection);
 
-var path = d3.geo.path()
-        .projection(projection);
-
-var svg = d3.select("#map").append("svg")
-        .attr("height", h)
-        .attr("width", fill)
+// var svg = d3.select("#map").append("svg")
+//         .attr("height", 10)
+//         .attr("width", 10)
+//         .attr("class", "svg")
 
 
-var svg1 = d3.select("#bar").append("svg")
+var svg1 = d3.select("#checkbox").append("svg")
         .attr("height", h)
         .attr("width", fill);
 
@@ -28,5 +31,65 @@ var svg2 = d3.select("#flower").append("svg")
         .attr("height", h)
         .attr("width", fill);
 
+
+var basic = new Datamap({
+      element: document.getElementById("map")
+  });
+d3.select(".datamap")
+    .attr("width", "100%")
+    .attr("transform", "translate(" + -50 + "," + -50 + ")")
+    .attr("viewBox","-50 150 1100 250")
+d3.select("path")
+    .attr("height", "100%")
+
+var year1 = 2003
+
+d3.select("#slider").insert("p", ":first-child").append("input")
+    .attr("type", "range")
+    .attr("min", "2003")
+    .attr("max", "2016")
+    .attr("value", year1)
+    .attr("id", "year");
+
+var title = d3.select("#slider").insert("h1", ":first-child")
+title.text(year1)
+dataBar = dataBar
+d3.selectAll('.datamaps-subunit').on("click", function(d) {
+
+
+      data1 = dataBar[String(year1)][d["properties"]["name"]]
+      console.log(d["properties"]["name"]);
+      console.log(data1);
+      // updatebar(data1, , year1, x, y)
+      // d3.event.stopPropagation();
+      });
+
+console.log(year1);
+// var basic_choropleth = new Datamap({
+//   element: document.getElementById("map"),
+//   projection: 'mercator',
+//   fills: {
+//     defaultFill: "#ABDDA4",
+//     authorHasTraveledTo: "#fa0fa0"
+//   },
+//   data: {
+//     USA: { fillKey: "authorHasTraveledTo" },
+//     JPN: { fillKey: "authorHasTraveledTo" },
+//     ITA: { fillKey: "authorHasTraveledTo" },
+//     CRI: { fillKey: "authorHasTraveledTo" },
+//     KOR: { fillKey: "authorHasTraveledTo" },
+//     DEU: { fillKey: "authorHasTraveledTo" },
+//   }
+// });
+//
+// var colors = d3.scale.category10();
+//
+//
+// window.setInterval(function() {
+//   d3.select("datamap").attr("width", "60%");
+//   basic_choropleth.updateChoropleth({
+//
+//   });
+// }, 2000);
 
 }
