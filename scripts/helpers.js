0df79c632fca734b7d635772e96e3a9959dd78a2
daanuittenhout <daanuittenhout@gmail.com>
2018-01-29 14:44:32 +0100

    up

---
+++ scripts/helpers.js
@@ -0,0 +1,149 @@
+
+
+  function updatebar(data1, country, year1, x, y, remove) {
+
+    d3.selectAll(".yaxis").remove()
+    d3.selectAll(".bar").remove()
+    d3.selectAll(".xaxis").remove()
+
+    dataPilars = []
+    dataList = []
+    dataEstimates = []
+    dataAbr = ["Lack of Terrorism", "Lack of Corruption", "Effectiveness(Government)", "Regulatory Quality", "Rule of Law"]
+    dataEstimates.push(Object.values(data1))
+    dataPilars.push(Object.getOwnPropertyNames(data1))
+
+    for (var i = 0; i < dataEstimates[0].length; i++) {
+      if (isNaN(dataEstimates[0][i])) {
+        dataList = []
+      } else {
+        dataList.push(parseFloat(dataEstimates[0][i]))
+      }
+    }
+    // Scale the range of the data in the domains
+    x.domain(dataAbr.map(function(d) {
+      // if dataList.values;
+      return d;
+    }));
+    y.domain([-3, 3])
+
+    svg3.selectAll("bar")
+      .data(dataList)
+      .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d, i) {
+        return 120 + (i * 107.5)
+      })
+      .attr("y", function(d, i) {
+        if (d < 0) {
+          return 300;
+        } else {
+          return 300 - (d * 84);
+        }
+      })
+      .attr("height", function(d, i) {
+        return Math.abs(d * 84);
+      })
+      .attr("width", 30)
+      .style("fill", function(d) {
+        if (d < 0) {
+          return "red";
+        } else {
+          return "green"
+        }
+
+      })
+
+
+    var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left");
+
+    svg3.append("g")
+      .attr("width", "100")
+      .attr("class", 'xaxis')
+      .attr("transform", "translate(50," + 300 + ")")
+      .call((xAxis));
+
+    // add the y Axis
+    svg3.append("g")
+      .attr("width", "100%")
+      .attr("class", 'yaxis')
+      .attr("transform", "translate(50," + 50 + ")")
+      .call((yAxis));
+
+
+  }
+
+  function tableCreate() {
+    var body = document.getElementById("checkboxes")
+    var tbl = document.createElement('table');
+    tbl.style.width = '100%';
+    tbl.style.height = '25%';
+    tbl.setAttribute('border', '1');
+    var tbdy = document.createElement('tbody');
+    for (var i = 0; i < 10; i++) {
+      var tr = document.createElement('tr');
+      for (var j = 0; j < 6; j++) {
+        if (i == 0) {
+          var th = document.createElement('th');
+          th.appendChild(document.createTextNode('\u0020'))
+        } else if (i == 10 && j == 6) {
+          break
+        } else {
+          var td = document.createElement('td');
+          td.setAttribute("id", "id" + String(i) + String(j))
+          td.appendChild(document.createTextNode('\u0020'))
+          // i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
+          tr.appendChild(td)
+        }
+
+        // td.append(document.getElementById("checkboxes").appendChild(newInput))
+      }
+      tbdy.appendChild(tr);
+    }
+    tbl.appendChild(tbdy);
+    body.appendChild(tbl)
+  }
+
+  function radiobutt() {
+
+
+    for (var p = 0; p < 9; p++) {
+
+      for (var q = 0; q < 5; q++) {
+
+        newInput = document.createElement("INPUT");
+        newInput.setAttribute("type", "radio");
+        newInput.setAttribute("name", "optradio" + p);
+        newInput.setAttribute("value", q + 1);
+        newInput.setAttribute("id", "radio" + p + q)
+        newInput.setAttribute("class", "radio")
+        document.getElementById("id" + String(p + 1) + String(q + 1)).append(newInput)
+      }
+    }
+  }
+
+  function flatten(arr) {
+    return arr.reduce(function(flat, toFlatten) {
+      return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+    }, []);
+  }
+
+  function sortProperties(obj) {
+    // convert object into array
+    var sortable = [];
+    for (var key in obj)
+      if (obj.hasOwnProperty(key))
+        sortable.push([key, obj[key]]); // each item is an array in format [key, value]
+
+    // sort items by value
+    sortable.sort(function(a, b) {
+      return b[1] - a[1]; // compare numbers
+    });
+    return sortable; // array in format [ [ key1, val1 ], [ key2, val2 ], ... ]
+  }
---
+++ scripts/tax0.js
@@ -0,0 +1,263 @@
+window.onload = function(e) {
+
+  h = 500,
+  w = 1000,
+  fill = "100%",
+  height = 500,
+  width = 300;
+
+  x = d3.scale.ordinal().rangeRoundBands([0, 300], 1);
+
+  y = d3.scale.linear().range([400, 0]);
+
+  svg = d3.select("#map1").append("svg")
+    .attr("height", 900)
+    .attr("width", 0)
+    .attr("class", "svg2")
+
+  svg1 = d3.select("#checkbox").append("svg")
+    .attr("height", h)
+    .attr("width", fill);
+
+  svg2 = d3.select("#flower").append("svg")
+    .attr("height", h)
+    .attr("width", fill)
+    .attr("id", "pie")
+    .append('g') //create a group to hold our pie chart
+    .attr('transform', 'translate(' + (width / 2) +
+      ',' + (150) + ')'); //move the center of the pie chart from 0, 0 to specified value
+
+  svg3 = d3.select("#bar").append("svg")
+    .attr("height", 600)
+    .attr("width", fill);
+
+  svg4 = d3.select("#bar1").append("svg")
+    .attr("height", 600)
+    .attr("width", fill);
+
+  x = d3.scale.ordinal()
+    .rangeRoundBands([0, 600], .5);
+
+  y = d3.scale.linear()
+    .range([h, 0]);
+
+  radius = 300 / 2; //radius of the pie-chart
+  d3.select("path")
+    .attr("height", "100%")
+
+  year1 = 2003
+
+  d3.select("#slider").insert("p", ":first-child").append("input")
+    .attr("type", "range")
+    .attr("min", "2003")
+    .attr("max", "2016")
+    .attr("value", year1)
+    .attr("id", "year");
+
+  var title = d3.select("#slider").insert("h1", ":first-child")
+  title.text(year1)
+
+  d3.json("./data/corTax1.json", function(data) {
+
+    var map = new Datamap({
+      element: document.getElementById("map2"),
+      projection: 'mercator',
+      fills: {
+        defaultFill: "grey"
+      },
+
+      geographyConfig: {
+        highlightBorderColor: '#bada55',
+        popupTemplate: function(geography, d) {
+          ctr = "not known"
+          if (typeof data[geography.properties.name] !== "undefined") {
+            dummy = data[geography.properties.name]["Tax"]
+            if (typeof x !== "undefined") {
+              ctr = String(dummy) + ("%")
+            }
+          }
+          return ['<div class="hoverinfo">',
+            '<strong>', geography.properties.name, '</strong>',
+            '<br>Tax rate: <strong>', ctr, '</strong>',
+            '</div>'
+          ].join('');
+        },
+        highlightBorderWidth: 3
+      }
+    });
+    d3.json("./data/colors1.json", function(data) {
+      map.updateChoropleth(data)
+    })
+  })
+  d3.selectAll('#herebutton').on("click", function(d) {
+    $('html,body').animate({
+      scrollTop: $("#header").offset().top
+    }, 'slow')
+  })
+
+
+  d3.json("../data/dataBar1.json", function(data) {
+    dataBar = data
+    d3.select("#year").on("input", function() {
+      year1 = this.value
+      if (typeof country != "undefined") {
+        title.text(year1 + " " + country)
+      } else {
+        title.text(year1)
+      }
+      if (typeof countrycode != "undefined") {
+        data1 = data[String(year1)][String(countrycode)]
+        updatebar(data1, country, year1, x, y)
+      }
+    })
+    d3.selectAll('.datamaps-subunit').on("click", function(d, dataBar) {
+      $('html,body').animate({
+        scrollTop: $("#slider").offset().top
+      }, 'slow')
+      if (d["id"] != "") {
+        country = d["properties"]["name"]
+        console.log(country);
+        countrycode = d["id"];
+        data1 = data[String(year1)][String(countrycode)]
+        updatebar(data1, country, year1, x, y)
+        title.text(year1 + " " + country)
+
+      }
+    });
+  });
+  tableCreate()
+  for (var i = 0; i < 10; i++) {
+    d3.selectAll("#id" + i + 0).append("text").text(String(i))
+  }
+  radiobutt()
+
+  d3.select("#resetbutton").on("click", function(d) {
+    d3.selectAll(".piechart").remove()
+    d3.selectAll(".radio").remove()
+    d3.selectAll("#match1").remove()
+    radiobutt()
+  })
+  d3.select("#gobutton").on("click", function(d) {
+    d3.selectAll(".piechart").remove()
+    d3.selectAll("#match1").remove()
+
+    var count = 0;
+    lists = ["Minwage", "English", "ChildLabour", "Environment", "Education",
+      "Inovation", "Infrastructure", "ICTAcces", "TaxRate"
+    ]
+
+    for (var i = 0; i < 46; i++) {
+      if (document.getElementsByTagName("INPUT")[i].checked == true) {
+        count++
+      }
+    }
+    if (count < 9) {
+      alert("Please fill in all the questions");
+    } else {
+      $('html,body').animate({
+        scrollTop: $("#flower").offset().top
+      }, 'slow')
+      d3.json("./data/ranks.json", function(data) {
+        results = []
+        r = 0
+        for (var i = 0; i < 45; i++) {
+          if (i % 5 == 0) {
+            r++
+            // console.log(r, i);
+          }
+          if (document.getElementsByTagName("INPUT")[i].checked == true) {
+            results.push(data[lists[r - 1]][document.getElementsByTagName("INPUT")[i].getAttribute("value")])
+          }
+        }
+        for (var i = 35; i < 40; i++) {
+          if (document.getElementsByTagName("INPUT")[i].checked == true) {
+            results.push(data[lists[7]][document.getElementsByTagName("INPUT")[i].getAttribute("value")])
+          }
+        }
+
+
+        allResults = flatten(results)
+
+        var counts = {}; //We are going to count occurrence of item here
+        var compare = 0; //We are going to compare using stored value
+        var mostFrequent;
+        var counter = 0
+
+        for (var i = 0, len = allResults.length; i < len; i++) {
+          var word = allResults[i];
+          if (counts[word] === undefined) { //if count[word] doesn't exist
+            counts[word] = 1; //set count[word] value to 1
+          } else { //if exists
+            counts[word] = counts[word] + 1; //increment existing value
+          }
+          if (counts[word] > compare) { //counts[word] > 0(first time)
+            compare = counts[word]; //set compare to counts[word]
+            mostFrequent = allResults[i]; //set mostFrequent value
+          }
+        }
+
+        var total = 0;
+        var sortedCountries = sortProperties(counts)
+        for (var i = 0; i < sortedCountries.slice(0, 5).length; i++) {
+          total += sortedCountries.slice(0, 5)[i][1]
+        }
+        countriestop = []
+
+        percentages = []
+        for (var i = 0; i < sortedCountries.slice(0, 5).length; i++) {
+          percentages.push((sortedCountries.slice(0, 5)[i][1] / total) * 100)
+          countriestop.push(sortedCountries.slice(0, 5)[i][0])
+        }
+
+        var colour = d3.scale.category20c();
+        var tip = d3.tip()
+          .attr('class', 'd3-tip')
+          .offset([-10, 0])
+          .html(function(d, i) {
+            return "<strong>Country:</strong> <span style='color:red'>" + countriestop[i] + "</span>";
+          })
+        var tip1 = d3.tip()
+          .attr('class', 'd3-tip')
+          .offset([-100, 0])
+          .html(function(d, i) {
+            return "<strong>hoi:</strong> <span style='color:red'>" + Math.round(((sortedCountries.slice(0, 5)[i][1] / 9) * 100), 2) + "</span>";
+          })
+        var arc = d3.svg.arc().outerRadius(radius);
+        var pie = d3.layout.pie()
+          .value(function(d, i) {
+            return percentages[i];
+          })
+          .sort(null);
+        svg.call(tip);
+        svg.call(tip);
+
+        var path = svg2.selectAll("path")
+          .data(pie(percentages))
+          .enter()
+          .append('path')
+          .attr("class", "piechart")
+          .attr('d', arc)
+          .attr('fill', function(d, i) {
+            return colour(i);
+          })
+          .on('mouseover', tip.show)
+          .on('mouseout', tip.hide)
+          .on("click", function(d, i) {
+            // d3.selectAll("#fillgauge1").remove()
+            d3.selectAll("#match1").remove()
+
+            var gauge1 = loadLiquidFillGauge("fillgauge1", Math.round(((sortedCountries.slice(0, 5)[i][1] / 9) * 100), 2));
+            var config1 = liquidFillGaugeDefaultSettings();
+            config1.circleColor = "#FF7777";
+            config1.textColor = "#FF4444";
+            config1.waveTextColor = "#FFAAAA";
+            config1.waveColor = "#FFDDDD";
+            config1.circleThickness = 0.2;
+            config1.textVertPosition = 0.2;
+            config1.waveAnimateTime = 1000;
+            return countriestop[i];
+          })
+      })
+    }
+  })
+}
